d12685bddcbdb992cf3495155510a465b8520f63
Hichem <benabadji.hichem@gmail.com>
2023-08-06 12:45:35 +0100

    Edit

--- build/app/generated/source/buildConfig/release/com/example/adv_basics/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/example/adv_basics/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.adv_basics;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.example.adv_basics";
+  public static final String APPLICATION_ID = "com.hichembenabadji.FootbalQuiz";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0.1";
